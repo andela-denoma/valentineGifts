8c3052b701af69e8c6029297be1ce12b215e1af8
Enoma Deborah <deborah.enomah@andela.co>
2015-01-08 15:50:10 +0100

    modified

--- route/gift.js
+++ route/gift.js
@@ -5,31 +5,6 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/valentine');
-console.log('mongoose');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log('...connected');
-});
-
-// var gifts = [
-//   {
-//     'name': 'Bag',
-//     'description': 'Gucci and Gabarna',
-//     'price': 3000
-//   },
-//   {
-//     'name': 'Shoe',
-//     'description': 'All Star',
-//     'price': 2000
-//   }
-// ];
-
-// app.use('/api', router);
-
 var Gift = require('../model/gifts');
 
 router.route('/users/gifts')
@@ -41,8 +16,6 @@ router.route('/users/gifts')
     description: req.body.description,
     price: req.body.price
   });
-
-  
 
   gift.save(function(err){
 
@@ -103,51 +76,4 @@ router.route('/users/gifts/:gifts_id')
   });
 });
 
-
-// .get(function(req, res, next) {
-//   res.json(gifts);
-// })
-
-
-// .post(function(req, res){
-//   gifts.push(req.body);
-
-//   res.status(201).json(gifts);
-// });
-
-// router.route('/users/gifts/:name')
-
-// .put(function (req, res) {
-//   var name = req.params.name;
-//    var editGifts = req.body;
-//    console.log(editGifts);
-//    for(i = 0; i < gifts.length; i++){
-//      if(gifts[i].name === name) {
-//        gifts[i].name = editGifts.name || gifts[i].name;
-//        gifts[i].description = editGifts.description || gifts[i].description;
-//        gifts[i].price = editGifts.price || gifts[i].price;
-
-      
-//      }
-//      else {
-//        res.send("No data found");
-//      }
-//    }
-//     res.status(200).json(gifts);
-//  })
-
-// .delete(function(req, res){
-//    for(i = 0; i < gifts.length; i++){
-//      if(req.params.name === gifts[i].name) {
-//        gifts.splice(i, 1);
-       // gifts[i].name = deleteGifts.name || gifts[i].name;
-       // gifts[i].description = deleteGifts.description || gifts[i].description;
-       // gifts[i].price = deleteGifts.price || gifts[i].price;
-
-      
-//      }
-// }
-
-// res.json(gifts);
-//  });
 module.exports = router;
