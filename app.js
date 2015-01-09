f8063051a4ddafeee28ba21574ceaca436ab0985
gbolly <gbolahan.okerayi@andela.co>
2015-01-09 14:36:29 +0100

    update

--- app.js
+++ app.js
@@ -5,15 +5,6 @@
 
 var express = require('express');   //call express
 var app = express();                //define our app using express
-var gift = require('./route/gift');
-
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /
-app.use('/', gift);
-
-app.set('port', (process.env.PORT || 3000)); //set our port
-
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -23,6 +14,16 @@ var allowCrossDomain = function(req, res, next) {
   };
 
   app.use(allowCrossDomain);
+
+var gift = require('./route/gift');
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /
+app.use('/', gift);
+
+app.set('port', (process.env.PORT || 3000)); //set our port
+
 
 // START THE SERVER
 
