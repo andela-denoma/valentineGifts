c93a57757c3d1becbd878e648bd6e9f3d9e30186
Enoma Deborah <deborah.enomah@andela.co>
2015-01-09 12:21:02 +0100

    modified

--- route/gift.js
+++ route/gift.js
@@ -53,13 +53,13 @@ router.route('/users/gifts')
 
 .get(function(req, res){
 
-  Gift.find(function(err, all_gift){
+  // Gift.find(function(err, all_gift){
 
-    if(err){
-      res.send(err);
-    }
-    res.json(all_gift);
-  });
+  //   if(err){
+  //     res.send(err);
+  //   }
+  //   res.json(all_gift);
+  // });
   //removes the -v and _id that comes by default when a gift is created
   Gift.find({}, 'name description price -_id', function (err, all_gift) {
      if(err){
