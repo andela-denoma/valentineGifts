dd03accc344d332f2f00773bfbe018bfc5d62907
Enoma Deborah <deborah.enomah@andela.co>
2015-01-09 11:05:40 +0100

    added allow cross domain access

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 var gift = require('./route/gift');
-app.use('/api', gift);
 
 app.use('/', gift);
 
 app.set('port', (process.env.PORT || 3000));
+
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  };
+
+  app.use(allowCrossDomain);
 
 app.listen(app.get('port'), function(){
  console.log('Server Started');
