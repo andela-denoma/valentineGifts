7b55dde864945365b34e56658302e5eb5937dde4
gbolly <gbolahan.okerayi@andela.co>
2015-01-09 14:22:30 +0100

    update

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.set('port', (process.env.PORT || 3000)); //set our port
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
   };
 
   app.use(allowCrossDomain);
